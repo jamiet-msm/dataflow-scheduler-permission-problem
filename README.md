0fb215e6e9ce87e23805f27a976d98fd9838689c
Jamie Thomson <jamie.thomson@moneysupermarket.com>
2021-12-16 17:25:09 +0000

    Cloud scheduler job to create the dataflow job

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ To run the demo, issue:
 ```bash
 export PROJECT=your_gcp_project
 export REGION=your_gcp_region #e.g. europe-west2, us-central1, etc...
-export NETWORK=your_network
-export SUBNETWORK=your_subnetwork
 docker run -it -v $(pwd):/tf -w /tf hashicorp/terraform init 
 docker run -it -v ~/.config/gcloud:/root/.config/gcloud \
         -v $(pwd):/tf \
@@ -16,7 +14,11 @@ docker run -it -v ~/.config/gcloud:/root/.config/gcloud \
         hashicorp/terraform apply \
         -var project=${PROJECT} \
         -var region=${REGION} \
-        -var network=${NETWORK} \
-        -var subnetwork=${SUBNETWORK} \
         -auto-approve
+```
+
+If the Dataflow job runs successfully then there should be some data in the BigQuery table. You cna check this by issuing:
+
+```bash
+bq query --nouse_legacy_sql "select count(*) from ${PROJECT}.dataflow_demo.t"
 ```
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 
 provider "google" {
   project = var.project
-  region = var.region
+  region  = var.region
 }
 
 
@@ -22,17 +22,16 @@ provider "google" {
 variable "project" {}
 variable "region" {}
 
-variable "network" {
-    default = "default"
-}
-variable "subnetwork" {
-    default = ""
+
+resource "google_compute_network" "network" {
+  name = "dataflow-demo"
+  auto_create_subnetworks = true
 }
 
 resource "google_storage_bucket" "bucket" {
-  name          = "${var.project}-df-demo"
-  location      = "EU"
-  force_destroy = true
+  name                        = "${var.project}-df-demo"
+  location                    = "EU"
+  force_destroy               = true
   uniform_bucket_level_access = true
 }
 resource "google_storage_bucket_object" "transform_js" {
@@ -42,10 +41,10 @@ resource "google_storage_bucket_object" "transform_js" {
 }
 
 resource "google_storage_bucket_object" "bqtable_schema_json" {
-  name   = "bqtable-schema.json"
-#   source = "./bqtable-schema.json"
+  name = "bqtable-schema.json"
+  #   source = "./bqtable-schema.json"
   content = "{\"BigQuery Schema\": ${file("${path.module}/bqtable-schema.json")}}"
-  bucket = google_storage_bucket.bucket.name
+  bucket  = google_storage_bucket.bucket.name
 }
 
 resource "google_storage_bucket_object" "lorem_ipsum" {
@@ -55,52 +54,104 @@ resource "google_storage_bucket_object" "lorem_ipsum" {
 }
 
 resource "google_bigquery_dataset" "bqdataset" {
-    dataset_id = "dataflow_demo"
+  dataset_id = "dataflow_demo"
 }
-resource "google_bigquery_table" "bqtable"{
-    dataset_id = google_bigquery_dataset.bqdataset.dataset_id
-    table_id = "t"
-    schema = file("${path.module}/bqtable-schema.json")
-    deletion_protection = false
+resource "google_bigquery_table" "bqtable" {
+  dataset_id          = google_bigquery_dataset.bqdataset.dataset_id
+  table_id            = "t"
+  schema              = file("${path.module}/bqtable-schema.json")
+  deletion_protection = false
 }
 
 resource "google_service_account" "sa" {
-    account_id   = "dataflow-demo"
+  account_id = "dataflow-demo"
 }
 
 resource "google_project_iam_member" "df_worker" {
-  role    = "roles/dataflow.worker"
-  member  = "serviceAccount:${google_service_account.sa.email}"
+  role   = "roles/dataflow.worker"
+  member = "serviceAccount:${google_service_account.sa.email}"
 }
 
 resource "google_project_iam_member" "storage_admin" {
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.sa.email}"
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.sa.email}"
 }
 
 resource "google_project_iam_member" "bq_admin" {
-  role    = "roles/bigquery.admin"
-  member  = "serviceAccount:${google_service_account.sa.email}"
+  role   = "roles/bigquery.admin"
+  member = "serviceAccount:${google_service_account.sa.email}"
 }
 
 
-resource "google_dataflow_job" "big_data_job" {
-  name              = "dataflow-job"
-  template_gcs_path = "gs://dataflow-templates/latest/GCS_Text_to_BigQuery"
+resource "google_dataflow_job" "dataflow_job" {
+  name                  = "dataflow-job"
+  template_gcs_path     = "gs://dataflow-templates/latest/GCS_Text_to_BigQuery"
   service_account_email = google_service_account.sa.email
-  temp_gcs_location = "gs://${google_storage_bucket.bucket.id}/tmp_dir"
-  network = var.network
-  subnetwork = "regions/${var.region}/subnetworks/${var.subnetwork}"
+  temp_gcs_location     = "gs://${google_storage_bucket.bucket.id}/tmp_dir"
+  network               = google_compute_network.network.name
   parameters = {
-      javascriptTextTransformGcsPath = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.transform_js.name}"
-      JSONPath = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.bqtable_schema_json.name}"
-      javascriptTextTransformFunctionName = "transform"
-      outputTable = "${var.project}:${google_bigquery_table.bqtable.dataset_id}.${google_bigquery_table.bqtable.table_id}"
-      bigQueryLoadingTemporaryDirectory = "${google_storage_bucket.bucket.url}/bq_tmp_dir"
-      inputFilePattern = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.lorem_ipsum.name}"
+    javascriptTextTransformGcsPath      = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.transform_js.name}"
+    JSONPath                            = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.bqtable_schema_json.name}"
+    javascriptTextTransformFunctionName = "transform"
+    outputTable                         = "${var.project}:${google_bigquery_table.bqtable.dataset_id}.${google_bigquery_table.bqtable.table_id}"
+    bigQueryLoadingTemporaryDirectory   = "${google_storage_bucket.bucket.url}/bq_tmp_dir"
+    inputFilePattern                    = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.lorem_ipsum.name}"
   }
   on_delete = "cancel"
   depends_on = [
     google_project_iam_member.df_worker, google_project_iam_member.storage_admin, google_project_iam_member.bq_admin
   ]
 }
+
+locals {
+  df_base_uri               = "https://dataflow.googleapis.com/v1b3/projects/${var.project}/locations/${var.region}"
+  url_encoded_template_path = urlencode("gs://dataflow-templates/latest/GCS_Text_to_BigQuery")
+}
+resource "google_cloud_scheduler_job" "trigger_dataflow_job_via_api" {
+  name      = "aaa-schedule-dataflow-job" #appear at top of alphabetically ordered list
+  schedule  = "23 */6 * * *"
+  time_zone = "Europe/London"
+  http_target {
+    http_method = "POST"
+    uri         = "${local.df_base_uri}/templates:launch?gcsPath=${local.url_encoded_template_path}"
+
+    headers = {
+      "Content-Type" = "application/json"
+    }
+    body = base64encode(
+      jsonencode(
+        {
+          jobName = "dataflow-job-from-scheduler"
+          parameters = {
+            javascriptTextTransformGcsPath      = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.transform_js.name}"
+            JSONPath                            = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.bqtable_schema_json.name}"
+            javascriptTextTransformFunctionName = "transform"
+            outputTable                         = "${var.project}:${google_bigquery_table.bqtable.dataset_id}.${google_bigquery_table.bqtable.table_id}"
+            bigQueryLoadingTemporaryDirectory   = "${google_storage_bucket.bucket.url}/bq_tmp_dir"
+            inputFilePattern                    = "${google_storage_bucket.bucket.url}/${google_storage_bucket_object.lorem_ipsum.name}"
+          }
+          environment = {
+            tempLocation        = "gs://dataflow-templates/latest/GCS_Text_to_BigQuery"
+            network             = google_compute_network.network.name
+            serviceAccountEmail = google_service_account.sa.email
+          }
+        }
+      )
+    )
+    oauth_token {
+      service_account_email = google_service_account.sa.email
+    }
+  }
+}
+
+
+
+data "google_project" "project" {}
+
+  
+resource "google_project_iam_member" "sa_may_act_as_any_service_account" {
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.sa.email}"
+}
+
+
